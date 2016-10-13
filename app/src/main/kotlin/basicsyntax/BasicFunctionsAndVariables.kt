da67e6a84c2f640aa6c8a59b2bcdfef43d617a74
Vincent Brown <Vincent.Brown@intelematics.com>
2016-10-13 11:05:16 +1100

    Initial commit.

---
+++ app/src/main/kotlin/basicsyntax/BasicFunctionsAndVariables.kt
@@ -0,0 +1,58 @@
+package basicsyntax
+
+/**
+ * Created by vbrown on 12/10/2016.
+ */
+class BasicFunctionsAndVariables {
+
+    // Basic function.
+    fun basicFunction() {
+        println("Basic function")
+    }
+
+    // Function with parameter.
+    fun functionWithParam(param: String) {
+        println("Hello " + param)
+    }
+
+    // Function with nullable (?) parameter.
+    fun functionWithNullableParameter(param: String?){
+        if(param != null) {
+            println("Hello" + param)
+        }
+    }
+
+    // Function with inferred type and expression body.
+    fun sum(a: Int, b: Int) = a + b
+
+    // Function that returns no meaningful value (Unit is void equivalent).
+    fun doSomething(a: Int, b: Int): Unit {
+        println("Sum " + a + b)
+    }
+
+    fun localVariables() {
+        // Immutable.
+        val a: Int = 1
+        val b = 5 // Type is inferred.
+        val c: Int // Type is needed when no initilaiser is provided.
+        var d = 1 // Mutable with inferred type.
+    }
+
+    val a: Int = 5
+    val b: Int = 10
+
+    val max = if (a > b) a else b
+
+    // Function with 'when' expression - equivalent to 'switch'
+    fun whenExpression(a: Int) {
+        when(a) {
+            1 -> println("a == 1")
+            2 -> println("a == 2")
+            3 -> println("a == 3")
+            else -> {
+                println("a is niether 1, 2 or 3")
+            }
+        }
+    }
+
+}
